5c3126335f0f8c5fb6fc8e30439c208e08bbb056
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2024-12-27 09:28:17 +0800

    feat(pages): add our teams (desktop view) (#4)

---
+++ app/Livewire/Pages/OurTeam.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Component;
+
+class OurTeam extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.our-team');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Livewire\Index;
 use App\Livewire\Pages\About;
+use App\Livewire\Pages\OurTeam;
 use App\Livewire\Pages\Program;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Index::class)->name('landing');
 Route::get('/about', About::class)->name('about');
 Route::get('/our-program', Program::class)->name('our-program');
+Route::get('/our-teams', OurTeam::class)->name('our-team');
