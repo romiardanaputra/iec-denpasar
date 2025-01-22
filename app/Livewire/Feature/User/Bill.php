b8cf9d7f3896e1c976d64f319e86ae2b06579054
romiardanaputra <romiardana21@gmail.com>
2025-01-23 00:45:26 +0800

    feat: add bill page ui desktop mode

---
+++ app/Livewire/Feature/User/Bill.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Feature\User;
+
+use Livewire\Component;
+
+class Bill extends Component
+{
+    public function render()
+    {
+        return view('livewire.feature.user.bill');
+    }
+}
