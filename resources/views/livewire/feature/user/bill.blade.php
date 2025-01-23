95a9c4e2c70d24d4db92db2ba4701d08295f85c0
romiardanaputra <romiardana21@gmail.com>
2025-01-23 20:31:36 +0800

    refactor(bill): remove unused section layout

--- resources/views/livewire/feature/user/bill.blade.php
+++ resources/views/livewire/feature/user/bill.blade.php
@@ -1,129 +1,7 @@
 <x-app-layout>
   <div class="w-full p-6 mx-auto">
     <div class="flex flex-wrap -mx-3">
-      <div class="max-w-full px-3 lg:w-2/3 lg:flex-none">
-        <div class="flex flex-wrap -mx-3">
-          <div class="w-full max-w-full px-3 mb-4 xl:mb-0 xl:w-1/2 xl:flex-none">
-            <div
-              class="relative flex flex-col min-w-0 break-words bg-transparent border-0 border-transparent border-solid shadow-xl rounded-2xl bg-clip-border">
-              <div class="relative overflow-hidden rounded-2xl"
-                style="background-image: url('../assets/img/curved-images/curved14.jpg')">
-                <span
-                  class="absolute top-0 left-0 w-full h-full bg-center bg-cover bg-gradient-dark-gray opacity-80"></span>
-                <div class="relative z-10 flex-auto p-4">
-                  <i class="p-2 text-white fas fa-wifi"></i>
-                  <h5 class="pb-2 mt-6 mb-12 text-white">
-                    4562&nbsp;&nbsp;&nbsp;1122&nbsp;&nbsp;&nbsp;4594&nbsp;&nbsp;&nbsp;7852</h5>
-                  <div class="flex">
-                    <div class="flex">
-                      <div class="mr-6">
-                        <p class="mb-0 leading-normal text-white text-size-sm opacity-80">Card Holder</p>
-                        <h6 class="mb-0 text-white">Jack Peterson</h6>
-                      </div>
-                      <div>
-                        <p class="mb-0 leading-normal text-white text-size-sm opacity-80">Expires</p>
-                        <h6 class="mb-0 text-white">11/22</h6>
-                      </div>
-                    </div>
-                    <div class="flex items-end justify-end w-1/5 ml-auto">
-                      <img class="w-3/5 mt-2" src="../assets/img/logos/mastercard.png" alt="logo" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="w-full max-w-full px-3 xl:w-1/2 xl:flex-none">
-            <div class="flex flex-wrap -mx-3">
-              <div class="w-full max-w-full px-3 md:w-1/2 md:flex-none">
-                <div
-                  class="relative flex flex-col min-w-0 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
-                  <div
-                    class="p-4 mx-6 mb-0 text-center bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                    <div class="w-16 h-16 text-center bg-center icon bg-gradient-fuchsia shadow-soft-2xl rounded-xl">
-                      <i class="relative text-white opacity-100 fas fa-landmark text-size-xl top-31/100"></i>
-                    </div>
-                  </div>
-                  <div class="flex-auto p-4 pt-0 text-center">
-                    <h6 class="mb-0 text-center">Salary</h6>
-                    <span class="leading-tight text-size-xs">Belong Interactive</span>
-                    <hr class="h-px my-4 bg-transparent bg-gradient-horizontal-dark" />
-                    <h5 class="mb-0">+$2000</h5>
-                  </div>
-                </div>
-              </div>
-              <div class="w-full max-w-full px-3 mt-6 md:mt-0 md:w-1/2 md:flex-none">
-                <div
-                  class="relative flex flex-col min-w-0 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
-                  <div
-                    class="p-4 mx-6 mb-0 text-center bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                    <div class="w-16 h-16 text-center bg-center icon bg-gradient-fuchsia shadow-soft-2xl rounded-xl">
-                      <i class="relative text-white opacity-100 fab fa-paypal text-size-xl top-31/100"></i>
-                    </div>
-                  </div>
-                  <div class="flex-auto p-4 pt-0 text-center">
-                    <h6 class="mb-0 text-center">Paypal</h6>
-                    <span class="leading-tight text-size-xs">Freelance Payment</span>
-                    <hr class="h-px my-4 bg-transparent bg-gradient-horizontal-dark" />
-                    <h5 class="mb-0">$455.00</h5>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="max-w-full px-3 mb-4 lg:mb-0 lg:w-full lg:flex-none">
-            <div
-              class="relative flex flex-col min-w-0 mt-6 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
-              <div class="p-4 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
-                <div class="flex flex-wrap -mx-3">
-                  <div class="flex items-center flex-none w-1/2 max-w-full px-3">
-                    <h6 class="mb-0">Payment Method</h6>
-                  </div>
-                  <div class="flex-none w-1/2 max-w-full px-3 text-right">
-                    <a class="inline-block px-6 py-3 font-bold text-center text-white uppercase align-middle transition-all bg-transparent rounded-lg cursor-pointer leading-pro text-size-xs ease-soft-in shadow-soft-md bg-150 bg-gradient-dark-gray hover:shadow-soft-xs active:opacity-85 hover:scale-102 tracking-tight-soft bg-x-25"
-                      href="javascript:;"> <i class="fas fa-plus"> </i>&nbsp;&nbsp;Add New Card</a>
-                  </div>
-                </div>
-              </div>
-              <div class="flex-auto p-4">
-                <div class="flex flex-wrap -mx-3">
-                  <div class="max-w-full px-3 mb-6 md:mb-0 md:w-1/2 md:flex-none">
-                    <div
-                      class="relative flex flex-row items-center flex-auto min-w-0 p-6 break-words bg-transparent border border-solid shadow-none rounded-xl border-slate-100 bg-clip-border">
-                      <img class="mb-0 mr-4 w-1/10" src="../assets/img/logos/mastercard.png" alt="logo" />
-                      <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;7852</h6>
-                      <i class="ml-auto cursor-pointer fas fa-pencil-alt text-slate-700" data-target="tooltip_trigger"
-                        data-placement="top"></i>
-                      <div data-target="tooltip" class="hidden px-2 py-1 text-white bg-black rounded-lg text-size-sm">
-                        Edit Card
-                        <div
-                          class="invisible absolute h-2 w-2 bg-inherit before:visible before:absolute before:h-2 before:w-2 before:rotate-45 before:bg-inherit before:content-['']"
-                          data-popper-arrow></div>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="max-w-full px-3 md:w-1/2 md:flex-none">
-                    <div
-                      class="relative flex flex-row items-center flex-auto min-w-0 p-6 break-words bg-transparent border border-solid shadow-none rounded-xl border-slate-100 bg-clip-border">
-                      <img class="mb-0 mr-4 w-1/10" src="../assets/img/logos/visa.png" alt="logo" />
-                      <h6 class="mb-0">****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;****&nbsp;&nbsp;&nbsp;5248</h6>
-                      <i class="ml-auto cursor-pointer fas fa-pencil-alt text-slate-700" data-target="tooltip_trigger"
-                        data-placement="top"></i>
-                      <div data-target="tooltip" class="hidden px-2 py-1 text-white bg-black rounded-lg text-size-sm">
-                        Edit Card
-                        <div
-                          class="invisible absolute h-2 w-2 bg-inherit before:visible before:absolute before:h-2 before:w-2 before:rotate-45 before:bg-inherit before:content-['']"
-                          data-popper-arrow></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="w-full max-w-full px-3 lg:w-1/3 lg:flex-none">
+      <div class="w-full max-w-full px-3 lg:w-7/12 lg:flex-none">
         <color:div
           class="relative flex flex-col h-full min-w-0 break-words bg-white border-0 border-transparent border-solid shadow-soft-xl rounded-2xl bg-clip-border">
           <div class="p-4 pb-0 mb-0 bg-white border-b-0 border-b-solid rounded-t-2xl border-b-transparent">
@@ -206,79 +84,7 @@
           </div>
         </color:div>
       </div>
-    </div>
-
-    <div class="flex flex-wrap -mx-3">
-      <div class="w-full max-w-full px-3 mt-6 md:w-7/12 md:flex-none">
-        <div
-          class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
-          <div class="p-6 px-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
-            <h6 class="mb-0">Billing Information</h6>
-          </div>
-          <div class="flex-auto p-4 pt-6">
-            <ul class="flex flex-col pl-0 mb-0 rounded-lg">
-              <li class="relative flex p-6 mb-2 border-0 rounded-t-inherit rounded-xl bg-gray-50">
-                <div class="flex flex-col">
-                  <h6 class="mb-4 leading-normal text-size-sm">Oliver Liam</h6>
-                  <span class="mb-2 leading-tight text-size-xs">Company Name: <span
-                      class="font-semibold text-slate-700 sm:ml-2">Viking Burrito</span></span>
-                  <span class="mb-2 leading-tight text-size-xs">Email Address: <span
-                      class="font-semibold text-slate-700 sm:ml-2">oliver@burrito.com</span></span>
-                  <span class="leading-tight text-size-xs">VAT Number: <span
-                      class="font-semibold text-slate-700 sm:ml-2">FRB1235476</span></span>
-                </div>
-                <div class="ml-auto text-right">
-                  <a class="relative z-10 inline-block px-4 py-3 mb-0 font-bold text-center text-transparent uppercase align-middle transition-all border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 bg-gradient-red hover:scale-102 active:opacity-85 bg-x-25 bg-clip-text"
-                    href="javascript:;"><i
-                      class="mr-2 far fa-trash-alt bg-150 bg-gradient-red bg-x-25 bg-clip-text"></i>Delete</a>
-                  <a class="inline-block px-4 py-3 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 hover:scale-102 active:opacity-85 bg-x-25 text-slate-700"
-                    href="javascript:;"><i class="mr-2 fas fa-pencil-alt text-slate-700"
-                      aria-hidden="true"></i>Edit</a>
-                </div>
-              </li>
-              <li class="relative flex p-6 mt-4 mb-2 border-0 rounded-xl bg-gray-50">
-                <div class="flex flex-col">
-                  <h6 class="mb-4 leading-normal text-size-sm">Lucas Harper</h6>
-                  <span class="mb-2 leading-tight text-size-xs">Company Name: <span
-                      class="font-semibold text-slate-700 sm:ml-2">Stone Tech Zone</span></span>
-                  <span class="mb-2 leading-tight text-size-xs">Email Address: <span
-                      class="font-semibold text-slate-700 sm:ml-2">lucas@stone-tech.com</span></span>
-                  <span class="leading-tight text-size-xs">VAT Number: <span
-                      class="font-semibold text-slate-700 sm:ml-2">FRB1235476</span></span>
-                </div>
-                <div class="ml-auto text-right">
-                  <a class="relative z-10 inline-block px-4 py-3 mb-0 font-bold text-center text-transparent uppercase align-middle transition-all border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 bg-gradient-red hover:scale-102 active:opacity-85 bg-x-25 bg-clip-text"
-                    href="javascript:;"><i
-                      class="mr-2 far fa-trash-alt bg-150 bg-gradient-red bg-x-25 bg-clip-text"></i>Delete</a>
-                  <a class="inline-block px-4 py-3 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 hover:scale-102 active:opacity-85 bg-x-25 text-slate-700"
-                    href="javascript:;"><i class="mr-2 fas fa-pencil-alt text-slate-700"
-                      aria-hidden="true"></i>Edit</a>
-                </div>
-              </li>
-              <li class="relative flex p-6 mt-4 mb-2 border-0 rounded-b-inherit rounded-xl bg-gray-50">
-                <div class="flex flex-col">
-                  <h6 class="mb-4 leading-normal text-size-sm">Ethan James</h6>
-                  <span class="mb-2 leading-tight text-size-xs">Company Name: <span
-                      class="font-semibold text-slate-700 sm:ml-2">Fiber Notion</span></span>
-                  <span class="mb-2 leading-tight text-size-xs">Email Address: <span
-                      class="font-semibold text-slate-700 sm:ml-2">ethan@fiber.com</span></span>
-                  <span class="leading-tight text-size-xs">VAT Number: <span
-                      class="font-semibold text-slate-700 sm:ml-2">FRB1235476</span></span>
-                </div>
-                <div class="ml-auto text-right">
-                  <a class="relative z-10 inline-block px-4 py-3 mb-0 font-bold text-center text-transparent uppercase align-middle transition-all border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 bg-gradient-red hover:scale-102 active:opacity-85 bg-x-25 bg-clip-text"
-                    href="javascript:;"><i
-                      class="mr-2 far fa-trash-alt bg-150 bg-gradient-red bg-x-25 bg-clip-text"></i>Delete</a>
-                  <a class="inline-block px-4 py-3 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 hover:scale-102 active:opacity-85 bg-x-25 text-slate-700"
-                    href="javascript:;"><i class="mr-2 fas fa-pencil-alt text-slate-700"
-                      aria-hidden="true"></i>Edit</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="w-full max-w-full px-3 mt-6 md:w-5/12 md:flex-none">
+      <div class="w-full max-w-full px-3 md:w-5/12 md:flex-none">
         <div
           class="relative flex flex-col h-full min-w-0 mb-6 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
           <div class="p-6 px-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
@@ -402,6 +208,41 @@
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="flex flex-wrap -mx-3">
+      <div class="w-full max-w-full px-3 mt-6 md:w-full md:flex-none">
+        <div
+          class="relative flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
+          <div class="p-6 px-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
+            <h6 class="mb-0">Billing Information</h6>
+          </div>
+          <div class="flex-auto p-4 pt-6">
+            <ul class="flex flex-col pl-0 mb-0 rounded-lg">
+              <li class="relative flex p-6 mb-2 border-0 rounded-t-inherit rounded-xl bg-gray-50">
+                <div class="flex flex-col">
+                  <h6 class="mb-4 leading-normal text-size-sm">Oliver Liam</h6>
+                  <span class="mb-2 leading-tight text-size-xs">Company Name: <span
+                      class="font-semibold text-slate-700 sm:ml-2">Viking Burrito</span></span>
+                  <span class="mb-2 leading-tight text-size-xs">Email Address: <span
+                      class="font-semibold text-slate-700 sm:ml-2">oliver@burrito.com</span></span>
+                  <span class="leading-tight text-size-xs">VAT Number: <span
+                      class="font-semibold text-slate-700 sm:ml-2">FRB1235476</span></span>
+                </div>
+                <div class="ml-auto text-right">
+                  <a class="relative z-10 inline-block px-4 py-3 mb-0 font-bold text-center text-transparent uppercase align-middle transition-all border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 bg-gradient-red hover:scale-102 active:opacity-85 bg-x-25 bg-clip-text"
+                    href="javascript:;"><i
+                      class="mr-2 far fa-trash-alt bg-150 bg-gradient-red bg-x-25 bg-clip-text"></i>Delete</a>
+                  <a class="inline-block px-4 py-3 mb-0 font-bold text-center uppercase align-middle transition-all bg-transparent border-0 rounded-lg shadow-none cursor-pointer leading-pro text-size-xs ease-soft-in bg-150 hover:scale-102 active:opacity-85 bg-x-25 text-slate-700"
+                    href="javascript:;"><i class="mr-2 fas fa-pencil-alt text-slate-700"
+                      aria-hidden="true"></i>Edit</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </x-app-layout>
