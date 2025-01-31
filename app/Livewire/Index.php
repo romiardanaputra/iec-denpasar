a5ec341b4a42b16da70b4e7026a068fad1b4e93b
romiardanaputra <romiardana21@gmail.com>
2025-01-31 09:39:37 +0800

    feat: add seo on page for public page

--- app/Livewire/Index.php
+++ app/Livewire/Index.php
@@ -2,6 +2,9 @@
 
 namespace App\Livewire;
 
+use Artesaos\SEOTools\Facades\JsonLd;
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Livewire\Component;
 
 #[\Livewire\Attributes\Title('Landing')]
@@ -9,6 +12,26 @@ class Index extends Component
 {
     public function render()
     {
+        SEOMeta::setDescription('Tingkatkan kemampuan bahasa Inggris Anda bersama IEC Denpasar. Program kursus berkualitas untuk semua usia dan tingkat kemampuan.');
+        SEOMeta::addMeta('article:published_time', now()->toW3CString(), 'property');
+        SEOMeta::addMeta('article:section', 'Pendidikan', 'property');
+        SEOMeta::addKeyword(['kursus bahasa Inggris', 'IEC Denpasar', 'belajar bahasa Inggris', 'kursus terbaik Bali']);
+
+        OpenGraph::setDescription('Tingkatkan kemampuan bahasa Inggris Anda bersama IEC Denpasar. Program kursus berkualitas untuk semua usia dan tingkat kemampuan.');
+        OpenGraph::setTitle('Belajar Bahasa Inggris di IEC Denpasar | Kursus Berkualitas');
+        OpenGraph::setUrl('https://iecdenpasar.com');
+        OpenGraph::addProperty('type', 'article');
+        OpenGraph::addProperty('locale', 'id_ID');
+        OpenGraph::addProperty('locale:alternate', ['en_US', 'id_ID']);
+
+        OpenGraph::addImage('https://iecdenpasar.com/assets/img/banner.jpg');
+        OpenGraph::addImage('https://iecdenpasar.com/assets/img/classroom.jpg', ['height' => 300, 'width' => 300]);
+
+        JsonLd::setTitle('Belajar Bahasa Inggris di IEC Denpasar | Kursus Berkualitas');
+        JsonLd::setDescription('Tingkatkan kemampuan bahasa Inggris Anda bersama IEC Denpasar. Program kursus berkualitas untuk semua usia dan tingkat kemampuan.');
+        JsonLd::setType('EducationalOrganization');
+        JsonLd::addImage('https://iecdenpasar.com/assets/img/logo.png');
+
         return view('livewire.index');
     }
 }
--- app/Livewire/Pages/Program.php
+++ app/Livewire/Pages/Program.php
@@ -2,14 +2,37 @@
 
 namespace App\Livewire\Pages;
 
+use Artesaos\SEOTools\Facades\JsonLd;
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOMeta;
 use Livewire\Component;
 
-#[\Livewire\Attributes\Title('Program')]
+#[\Livewire\Attributes\Title('Program Kami')]
 
 class Program extends Component
 {
     public function render()
     {
+        SEOMeta::setDescription('Temukan berbagai program kursus bahasa Inggris di IEC Denpasar. Mulai dari anak-anak hingga profesional, kami menyediakan metode pembelajaran terbaik.');
+        SEOMeta::addMeta('article:published_time', now()->toW3CString(), 'property');
+        SEOMeta::addMeta('article:section', 'Program Kursus', 'property');
+        SEOMeta::addKeyword(['kursus bahasa Inggris Bali', 'program kursus IEC Denpasar', 'belajar bahasa Inggris', 'kursus anak dan dewasa']);
+
+        OpenGraph::setDescription('Temukan berbagai program kursus bahasa Inggris di IEC Denpasar. Mulai dari anak-anak hingga profesional, kami menyediakan metode pembelajaran terbaik.');
+        OpenGraph::setTitle('Program Kursus Bahasa Inggris di IEC Denpasar | Semua Level');
+        OpenGraph::setUrl('https://iecdenpasar.com/program');
+        OpenGraph::addProperty('type', 'article');
+        OpenGraph::addProperty('locale', 'id_ID');
+        OpenGraph::addProperty('locale:alternate', ['en_US', 'id_ID']);
+
+        OpenGraph::addImage('https://iecdenpasar.com/assets/img/program-banner.jpg');
+        OpenGraph::addImage('https://iecdenpasar.com/assets/img/classroom.jpg', ['height' => 300, 'width' => 300]);
+
+        JsonLd::setTitle('Program Kursus Bahasa Inggris di IEC Denpasar | Semua Level');
+        JsonLd::setDescription('Temukan berbagai program kursus bahasa Inggris di IEC Denpasar. Mulai dari anak-anak hingga profesional, kami menyediakan metode pembelajaran terbaik.');
+        JsonLd::setType('EducationalOrganization');
+        JsonLd::addImage('https://iecdenpasar.com/assets/img/logo.png');
+
         return view('livewire.pages.program');
     }
 }
