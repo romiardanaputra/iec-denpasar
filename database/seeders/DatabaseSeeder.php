e6bf8c4e482ee42258833dd3904e9d1fd5d2f71c
romiardanaputra <romiardana21@gmail.com>
2025-02-01 21:26:52 +0800

    refactor: add taggable to blog post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog\Author;
+use App\Models\Blog\Category as BlogCategory;
+use App\Models\Blog\Link;
+use App\Models\Blog\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Closure;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +20,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // User::factory(10)->create()
         $this->call([
             RoleSeeder::class,
         ]);
+
+        $this->command->warn(PHP_EOL.'Creating blog categories...');
+        $blogCategories = $this->withProgressBar(20, fn () => BlogCategory::factory(1)
+            ->count(20)
+            ->create());
+        $this->command->info('Blog categories created.');
+
+        $this->command->warn(PHP_EOL.'Creating blog authors and posts...');
+        $this->withProgressBar(20, fn () => Author::factory(1)
+            ->has(
+                Post::factory()->count(5)
+                    ->state(fn (array $attributes, Author $author) => ['blog_category_id' => $blogCategories->random(1)->first()->id]),
+                'posts'
+            )
+            ->create());
+        $this->command->info('Blog authors and posts created.');
+
+        $this->command->warn(PHP_EOL.'Creating blog links...');
+        $this->withProgressBar(20, fn () => Link::factory(1)
+            ->count(20)
+            ->create());
+        $this->command->info('Blog links created.');
+    }
+
+    protected function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
+    {
+        $progressBar = new ProgressBar($this->command->getOutput(), $amount);
+
+        $progressBar->start();
+
+        $items = new Collection;
+
+        foreach (range(1, $amount) as $i) {
+            $items = $items->merge(
+                $createCollectionOfOne()
+            );
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+
+        $this->command->getOutput()->writeln('');
+
+        return $items;
     }
 }
