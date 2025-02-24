fa59919bf254798c49b2caf6d6de2f455974c74e
romiardanaputra <romiardana21@gmail.com>
2025-02-24 12:51:49 +0800

    feat: add registration's schedule model and registration resource

---
+++ app/Filament/Resources/Feature/RegistrationScheduleResource/Pages/CreateRegistrationSchedule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Feature\RegistrationScheduleResource\Pages;
+
+use App\Filament\Resources\Feature\RegistrationScheduleResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRegistrationSchedule extends CreateRecord
+{
+    protected static string $resource = RegistrationScheduleResource::class;
+}
---
+++ app/Filament/Resources/Feature/RegistrationScheduleResource/Pages/EditRegistrationSchedule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Feature\RegistrationScheduleResource\Pages;
+
+use App\Filament\Resources\Feature\RegistrationScheduleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRegistrationSchedule extends EditRecord
+{
+    protected static string $resource = RegistrationScheduleResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
