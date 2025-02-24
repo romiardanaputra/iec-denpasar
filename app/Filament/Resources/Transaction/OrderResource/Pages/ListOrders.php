6f4b5d8837f363a32eeb237b95500e4ded328769
romiardanaputra <romiardana21@gmail.com>
2025-02-24 13:35:02 +0800

    feat: add admin's order resources

---
+++ app/Filament/Resources/Transaction/OrderResource/Pages/ListOrders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Transaction\OrderResource\Pages;
+
+use App\Filament\Resources\Transaction\OrderResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListOrders extends ListRecords
+{
+    protected static string $resource = OrderResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
