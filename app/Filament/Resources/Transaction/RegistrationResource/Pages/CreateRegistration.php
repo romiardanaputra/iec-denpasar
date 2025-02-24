6e4387f85af0c03f7c8536e199575fce3c2778af
romiardanaputra <romiardana21@gmail.com>
2025-02-24 12:53:16 +0800

    feat: add registration resource and registration's model

---
+++ app/Filament/Resources/Transaction/RegistrationResource/Pages/CreateRegistration.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Transaction\RegistrationResource\Pages;
+
+use App\Filament\Resources\Transaction\RegistrationResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRegistration extends CreateRecord
+{
+    protected static string $resource = RegistrationResource::class;
+}
