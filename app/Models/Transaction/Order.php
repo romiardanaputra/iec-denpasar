275b04951429cb4497993c559e738f811306c7c9
romiardanaputra <romiardana21@gmail.com>
2025-02-25 12:50:17 +0800

    refactor: add softdeletes in order model

--- app/Models/Transaction/Order.php
+++ app/Models/Transaction/Order.php
@@ -6,11 +6,10 @@ use App\Models\Program\Program;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'user_id',
