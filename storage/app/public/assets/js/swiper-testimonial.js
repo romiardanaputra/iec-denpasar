d25f5dcae45e599443ce365df9f37b17aa2578dd
romiardanaputra <romiardana21@gmail.com>
2025-03-04 23:10:24 +0800

    refactor: add storage and remove git ignore inside storage

---
+++ storage/app/public/assets/js/swiper-testimonial.js
@@ -0,0 +1,62 @@
+// resources/js/swiper-testimonial.js
+
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperTestimonial = new Swiper(".swiper-testimonial", {
+    modules: [Navigation, Pagination],
+    pagination: {
+      el: ".swiper-pagination-testimonial",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-testimonial",
+      prevEl: ".swiper-button-prev-testimonial",
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next-testimonial",
+          prevEl: ".swiper-button-prev-testimonial",
+        },
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next-testimonial",
+          prevEl: ".swiper-button-prev-testimonial",
+        },
+      },
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next-testimonial",
+          prevEl: ".swiper-button-prev-testimonial",
+        },
+      },
+    },
+  });
+});
